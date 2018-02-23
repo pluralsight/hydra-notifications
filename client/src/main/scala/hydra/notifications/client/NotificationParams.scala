c3146cf4c93d622c96baa5a7f2da53680a2f712c
McKay Harris <mckayharris11@gmail.com>
2018-02-23 12:44:22 -0700

    Added notification params class

---
+++ client/src/main/scala/hydra/notifications/client/NotificationParams.scala
@@ -0,0 +1,9 @@
+package hydra.notifications.client
+
+// $COVERAGE-OFF
+object NotificationParams {
+  /**
+    * Used to publish to a Slack channel other than the default channel.
+    */
+  val SlackNotificationChannelParam = "slack-notification-channel"
+}
