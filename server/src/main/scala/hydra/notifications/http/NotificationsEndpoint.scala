1df2db4ea0ff89f0cb971d78dbb48a4036cab2a2
Aman Minz <aman-minz@pluralsight.com>
2024-08-26 23:09:15 +0530

    Do not call opsgenie API for now

--- server/src/main/scala/hydra/notifications/http/NotificationsEndpoint.scala
+++ server/src/main/scala/hydra/notifications/http/NotificationsEndpoint.scala
@@ -54,7 +54,8 @@ class NotificationsEndpoint(notificationsSupervisor: ActorRef)
               val (title, description, details) = extractData(payload)
               val opsGenieNotification = OpsGenieNotification(title, priority, alias, description, noteOpt, team, tags, entity, sourceOpt, user, details)
 
-              notify(supervisor, opsGenieNotification)
+//              notify(supervisor, opsGenieNotification)
+              complete(OK, s"$message") // TODO: Added for testing. Remove later. Not invoking opsgenie API.
             } catch {
               case ex: DeserializationException => complete(StatusCodes.BadRequest, ex.getMessage)
             }
